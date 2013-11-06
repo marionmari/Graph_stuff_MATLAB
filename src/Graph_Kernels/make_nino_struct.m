7de4dde057147a5530c93348b46a76a5c442db10
marionmari <marion.neumann@iais.fraunhofer.de>
2013-11-06 17:09:57 +0100

    nino interface

--- src/Graph_Kernels/make_nino_struct.m
+++ src/Graph_Kernels/make_nino_struct.m
@@ -3,16 +3,31 @@ function Graphs = make_nino_struct(A, labels, graph_ind)
     Graphs = struct('am', {}, 'nl', {}, 'el', {}, 'al', {})
     
     for i=unique(graph_ind)
-
         
         ids = find(graph_ind==i);
-        Graphs(i).am = A(ids,ids);
+        num_nodes =  size(ids,1);
         
+        % am
+        curr_A = A(ids,ids);
+        Graphs(i).am = curr_A;
+        
+        % nl
+        node_list = struct(values, labels(ids,1));
+        Graphs(i).nl = node_list;
             
-        node_list
-        edge_list
-        adj_list
+        % el
+        [row, col] = find(curr_A);
+        val = ones(num_nodes,1);
+        edge_array =  [col, row, val];
+        edge_list = struct(values, edge_array);
+        Graphs(i).el = node_list;
         
+        % al
+        adj_list = cell(num_nodes,1);
+        for j=1:num_nodes
+            adj_list{j} = find(curr_A(j, : ));
+        end
+        Graphs(i).al = adj_list;
         
     end
     
